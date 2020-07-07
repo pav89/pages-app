c240bd736bfd3256e03761560ec35ef3aa167b25
pav89 <pav892018@gmail.com>
2020-07-08 01:26:14 +0300

    New updates for Heroku deployment

--- PagesSite/PagesSite/settings.py
+++ PagesSite/PagesSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wsb1!x9%6q7@bwqo0d@szox36vj_6i7&%c$1_k0)+1_^=@@hxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
